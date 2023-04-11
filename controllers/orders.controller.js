65adb7a935a327cc8a9a933a608f7c11e5284fc6
Geo <george.e.m.178@gmail.com>
2023-04-11 03:53:17 +0200

    Done - but still have some bugs (inserting new user data into database must fix with somthing like '.toLowercase' to avoid having some problem with capital letters G,g)

---
+++ controllers/orders.controller.js
@@ -0,0 +1,78 @@
+const stripe = require("stripe")(
+  "sk_test_51Muxw7GSK0vY2Y7MybUgAy45ShqPoMQpAcJHzNb4J6iGfuN2aioc2eOjT1wd8HRPsMK69zFi6bg6918M1bJyHQ9R00C959sRmZ"
+);
+
+const Order = require("../models/order.model");
+const User = require("../models/user.model");
+
+async function getOrders(req, res) {
+  try {
+    const orders = await Order.findAllForUser(res.locals.uid);
+    res.render("customer/orders/all-orders", {
+      orders: orders,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function addOrder(req, res, next) {
+  const cart = res.locals.cart;
+
+  let userDocument;
+  try {
+    userDocument = await User.findById(res.locals.uid);
+  } catch (error) {
+    return next(error);
+  }
+
+  const order = new Order(cart, userDocument);
+
+  try {
+    await order.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
+
+  req.session.cart = null;
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: cart.items.map((item) => {
+      return {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: item.product.title,
+          },
+          unit_amount: +item.product.price.toFixed(2) * 100,
+        },
+        quantity: item.quantity,
+      };
+    }),
+    mode: "payment",
+    success_url: `http://localhost:3000/orders/success`,
+    cancel_url: `http://localhost:3000/orders/failure`,
+  });
+
+  // 4242 4242 4242 4242 Succeeds payment
+  // 4000 0025 0000 3155 Payment require authentication
+  // 4000 0000 0000 9995 Declined payment
+
+  res.redirect(303, session.url);
+}
+
+function getSuccess(req, res) {
+  res.render("customer/orders/success");
+}
+
+function getFailure(req, res) {
+  res.render("customer/orders/failure");
+}
+
+module.exports = {
+  addOrder: addOrder,
+  getOrders: getOrders,
+  getSuccess: getSuccess,
+  getFailure: getFailure,
+};
